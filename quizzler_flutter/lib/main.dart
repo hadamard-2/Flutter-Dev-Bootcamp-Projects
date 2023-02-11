eb23394c821329ce26a47aca61ddfd15ef2168c0
Iyob G <104778355+hadamard-2@users.noreply.github.com>
2023-02-11 12:12:08 +0300

    - created a widget for the answer button

--- quizzler_flutter/lib/main.dart
+++ quizzler_flutter/lib/main.dart
@@ -28,25 +28,25 @@ class QuizPage extends StatefulWidget {
     {'You can lead a cow down stairs but not up stairs.': false},
     {'Approximately one quarter of human bones are in the feet.': true},
     {'A slug\'s blood is green.': true},
-    // {'A penguin is a bird that can fly.': false},
-    // {'The Great Wall of China is visible from the moon.': false},
-    // {
-    //   'The average person will swallow about eight spiders in their lifetime.':
-    //       false
-    // },
-    // {'The tallest mammal in the world is the giraffe.': true},
-    // {'The capital city of Australia is Sydney.': false},
-    // {
-    //   'The shortest war in history was between Britain and Zanzibar on 27 August 1896.':
-    //       true
-    // },
-    // {'The human nose can detect more than 1 trillion different scents.': true},
-    // {'Cats have better memories than dogs.': false},
-    // {
-    //   'The only letter that doesn\'t appear in any U.S. state name is \'Q\'.':
-    //       true
-    // },
-    // {'A group of flamingos is called a flamboyance.': true}
+    {'A penguin is a bird that can fly.': false},
+    {'The Great Wall of China is visible from the moon.': false},
+    {
+      'The average person will swallow about eight spiders in their lifetime.':
+          false
+    },
+    {'The tallest mammal in the world is the giraffe.': true},
+    {'The capital city of Australia is Sydney.': false},
+    {
+      'The shortest war in history was between Britain and Zanzibar on 27 August 1896.':
+          true
+    },
+    {'The human nose can detect more than 1 trillion different scents.': true},
+    {'Cats have better memories than dogs.': false},
+    {
+      'The only letter that doesn\'t appear in any U.S. state name is \'Q\'.':
+          true
+    },
+    {'A group of flamingos is called a flamboyance.': true}
   ];
 
   @override
@@ -75,7 +75,7 @@ class _QuizPageState extends State<QuizPage> {
     setState(() {
       bool answer = widget.myQuestions[questionIndex][currentQuestion];
       resultList.add(
-        PointIndicator(correctAnswer: answer == userResponse),
+        PointIndicator(isRight: answer == userResponse),
       );
     });
   }
@@ -93,45 +93,17 @@ class _QuizPageState extends State<QuizPage> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         QuestionCard(questionText: currentQuestion.toString()),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Container(
-              color: Colors.green,
-              child: TextButton(
-                onPressed: () {
-                  evaluateAnswerAndGoToNextQuestion(true);
-                },
-                child: const Text(
-                  'True',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-          ),
+        AnswerButton(
+          onPressedCallback: evaluateAnswerAndGoToNextQuestion,
+          buttonColor: Colors.green,
+          buttonText: 'True',
+          associatedAnswer: true,
         ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Container(
-              color: Colors.red,
-              child: TextButton(
-                onPressed: () {
-                  evaluateAnswerAndGoToNextQuestion(false);
-                },
-                child: const Text(
-                  'False',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-          ),
+        AnswerButton(
+          onPressedCallback: evaluateAnswerAndGoToNextQuestion,
+          buttonColor: Colors.red,
+          buttonText: 'False',
+          associatedAnswer: false,
         ),
         Row(
           children: resultList,
@@ -166,14 +138,52 @@ class QuestionCard extends StatelessWidget {
   }
 }
 
-class PointIndicator extends StatelessWidget {
-  final bool correctAnswer;
+class AnswerButton extends StatelessWidget {
+  final Function onPressedCallback;
+  final Color buttonColor;
+  final String buttonText;
+  final bool associatedAnswer;
 
-  const PointIndicator({super.key, required this.correctAnswer});
+  const AnswerButton(
+      {super.key,
+      required this.onPressedCallback,
+      required this.buttonColor,
+      required this.buttonText,
+      required this.associatedAnswer});
 
   @override
   Widget build(BuildContext context) {
-    if (correctAnswer) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Container(
+          color: buttonColor,
+          child: TextButton(
+            onPressed: () {
+              onPressedCallback(associatedAnswer);
+            },
+            child: Text(
+              buttonText,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PointIndicator extends StatelessWidget {
+  final bool isRight;
+
+  const PointIndicator({super.key, required this.isRight});
+
+  @override
+  Widget build(BuildContext context) {
+    if (isRight) {
       return const Icon(
         Icons.done,
         color: Colors.green,
