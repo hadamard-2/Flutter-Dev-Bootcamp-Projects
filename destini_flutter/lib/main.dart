f75c6a81159eedde5931da61e651cece3a34a434
Iyob G <104778355+hadamard-2@users.noreply.github.com>
2023-02-14 21:33:34 +0300

    - scrapped the whole project...
    and started over taking suggestions from the stub project's to-dos
    - sorta created a to-do list of my own
    - but most importantly, I learned how to set the color, padding, and border-radius of a TextButton

--- destini_flutter/lib/main.dart
+++ destini_flutter/lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'story_brain.dart';
-import 'story.dart';
 
 void main() => runApp(const MyApp());
-
-StoryBrain storyBrain = StoryBrain();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,20 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('images/sherlock.jpg'),
-              fit: BoxFit.cover,
-              opacity: 0.5,
-            ),
-          ),
-          child: const Padding(
-            padding: EdgeInsets.all(10.0),
-            child: StoryPage(),
-          ),
+      theme: ThemeData.dark(useMaterial3: true),
+      home: const Scaffold(
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: StoryPage(),
         ),
       ),
     );
@@ -40,73 +27,39 @@ class StoryPage extends StatefulWidget {
 }
 
 class _StoryPageState extends State<StoryPage> {
-  String option1Text = storyBrain.currentStory!.option1.toString();
-  bool option2ButtonVisible = true;
-
-  void reset() {
-    option2ButtonVisible = true;
-    storyBrain.currentStory = storyBrain.rootStory;
-  }
-
-  bool reachedTheEnd() {
-    return (storyBrain.currentStory!.option1 == null &&
-        storyBrain.currentStory!.option2 == null);
-  }
-
   @override
   Widget build(BuildContext context) {
-    if (reachedTheEnd()) {
-      option1Text = 'Restart';
-      option2ButtonVisible = false;
-    } else {
-      option1Text = storyBrain.currentStory!.option1.toString();
-    }
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Expanded(
-          flex: 5,
+        const Expanded(
+          flex: 12,
           child: Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15.0),
-              child: Text(
-                storyBrain.currentStory!.storyText,
-                textAlign: TextAlign.justify,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  height: 1.3,
-                ),
+            child: Text(
+              'This is where the story text goes.',
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
               ),
             ),
           ),
         ),
         Expanded(
+          flex: 2,
           child: TextButton(
-            onPressed: () {
-              setState(() {
-                if (reachedTheEnd()) {
-                  reset();
-                } else {
-                  storyBrain.choosePath(1);
-                }
-              });
-            },
-            child: Card(
-              color: const Color(0xFF8B0000),
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10.0,
-                    vertical: 5.0,
-                  ),
-                  child: Text(
-                    option1Text,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.red,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+            child: const Text(
+              'This where the first choice goes.',
+              style: TextStyle(
+                color: Colors.white,
               ),
             ),
           ),
@@ -114,36 +67,25 @@ class _StoryPageState extends State<StoryPage> {
         const SizedBox(
           height: 20,
         ),
-        Visibility(
-          visible: option2ButtonVisible,
-          child: Expanded(
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  storyBrain.choosePath(2);
-                });
-              },
-              child: Card(
-                color: const Color(0xFF0C50A1),
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 10.0,
-                      vertical: 5.0,
-                    ),
-                    child: Text(
-                      storyBrain.currentStory!.option2.toString(),
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                      ),
-                    ),
-                  ),
-                ),
+        Expanded(
+          flex: 2,
+          child: TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.blue,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+            child: const Text(
+              'This is where the second choice goes.',
+              style: TextStyle(
+                color: Colors.white,
               ),
             ),
           ),
-        )
+        ),
       ],
     );
   }
--- destini_flutter/lib/story.dart
+++ destini_flutter/lib/story.dart
@@ -1,9 +1,25 @@
 class Story {
-  String storyText;
-  String? option1;
-  String? option2;
-  Story? path1Story;
-  Story? path2Story;
+  final String _storyText;
+  final String _choice1;
+  final String _choice2;
 
-  Story({required this.storyText, this.option1, this.option2});
+  Story(
+      {required String storyText,
+      required String choice1,
+      required String choice2})
+      : _choice2 = choice2,
+        _choice1 = choice1,
+        _storyText = storyText;
+
+  String get storyText {
+    return _storyText;
+  }
+
+  String get choice1 {
+    return _choice1;
+  }
+
+  String get choice2 {
+    return _choice2;
+  }
 }
